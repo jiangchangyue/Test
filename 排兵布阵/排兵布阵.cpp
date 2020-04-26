b28c9ae680daa9c28d8921aac11ff00f86df8084
jiangchangyue <34598069+jiangchangyue@users.noreply.github.com>
2020-04-26 20:59:26 +0800

    Add files via upload

---
+++ 排兵布阵/排兵布阵.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<algorithm>
+#include<string.h>
+using namespace std;
+int n,m;
+int a[50005],c[50005];
+int lowbit(int x)
+{
+	return x&(-x);
+}
+void updata(int i,int k)
+{
+	while(i<=n)
+	{
+		c[i]+=k;
+		i+=lowbit(i);
+	}
+}
+int getsum(int i)
+{
+	int res=0;
+	while(i>0)
+	{
+		res+=c[i];
+		i-=lowbit(i);
+	}
+	return res;
+}
+
+int main(){
+    int t;
+    cin>>t;
+    for(int tot = 1; tot <= t; tot++)
+	{
+        cout << "Case " << tot << ":" << endl;
+        memset(a, 0, sizeof(a));
+        memset(c, 0, sizeof(c));
+        cin>>n;
+        for(int i = 1; i <= n; i++)
+		{
+            cin>>a[i];
+            updata(i,a[i]);   //输入初值的时候，也相当于更新了值
+        }
+        string s;
+        int x,y;
+        while(cin>>s && s[0] != 'E')
+		{
+            cin>>x>>y;
+            if(s[0] == 'Q')
+			{    //求和操作
+                int sum = getsum(y) - getsum(x-1);    //x-y区间和也就等于1-y区间和减去1-(x-1)区间和
+                cout << sum << endl;
+            }
+            else if(s[0] == 'A')
+			{
+                updata(x,y);
+            }
+            else if(s[0] == 'S')
+			{
+                updata(x,-y);    //减去操作，即为加上相反数
+            }
+        }
+    }
+    return 0;
+}
+
